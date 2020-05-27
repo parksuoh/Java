af27b35a2864508f3c02f0c6d18f18d4f4593737
parksuoh <sooho0163@naver.com>
2020-05-27 11:11:24 +0900

    Ch01 프로젝트 등록

---
+++ Ch01/src/sub1/Helloworld.java
@@ -0,0 +1,16 @@
+package sub1;
+/*
+ * 날짜 2020 04 29
+ * 이름 박수오
+ * 내용 자바 Helloworld 출력하기
+ * 
+ */
+
+public class Helloworld {
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+		System.out.println("Hello Java");
+	}
+	
+}
---
+++ Ch01/src/sub1/P17.java
@@ -0,0 +1,7 @@
+package sub1;
+
+public class P17 {
+	public static void main(String[] args) {
+		System.out.println("Hello, welcome to the java world!");
+	}
+}
